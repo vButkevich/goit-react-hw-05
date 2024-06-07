df83bedf24d6b3496521d614b62ceb8e2fff5034
vButkevich <vyacheslav.butkevich@gmail.com>
2024-06-07 21:24:25 +0300

    lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import React, { Suspense, lazy } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
 // import css from "./App.module.css";
 // import clsx from "clsx";
 
-// import About from "./pages/About/About";
-import HomePage from "./pages/Home/HomePage";
-import MoviesPage from "./pages/MoviesPage/MoviesPage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-import Navigation from "./components/Navigation/Navigation";
-
+const HomePage = lazy(() => import("./pages/Home/HomePage"));
+const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
+const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"));
+const Navigation = lazy(() => import("./components/Navigation/Navigation"));
 const MovieDetailsPage = lazy(() =>
   import("./pages/MovieDetailsPage/MovieDetailsPage")
 );
-const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"));
 const MovieReviews = lazy(() =>
   import("./components/MovieReviews/MovieReviews")
 );
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
+//const About = lazy(() => import("./pages/About/About"));
 
 export const App = () => {
   return (
